500bc8d9f5ad03ba580da1c2468dd72ebc1d7574
EyjiCuff <eyji.koike@gmail.com>
2022-08-31 17:53:23 -0300

    Created basic front ends

--- inventory_font_end/src/App.js
+++ inventory_font_end/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Products} from "./components/Products";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Create_Products} from "./components/Create_Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Products/>}/>
+            <Route path="/create" element={<Create_Products/>}/>
+        </Routes>
+    </BrowserRouter>;
 }
 
 export default App;
---
+++ orders_front_end/src/App.js
@@ -0,0 +1,13 @@
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Orders} from "./components/Orders";
+
+function App() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Orders/>}/>
+        </Routes>
+    </BrowserRouter>;
+}
+
+export default App;
